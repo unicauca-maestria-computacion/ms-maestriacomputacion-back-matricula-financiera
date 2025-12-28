38c52b1574ad80dbb556f795ac533930802d9eac
Alvaro Daniel Erazo Ramirez <alvaro.erazo@siigo.com>
2025-12-28 18:14:27 -0500

    creacion del micro matricula financiera

---
+++ src/main/java/co/edu/unicauca/matricula_financiera/MatriculaFinancieraApplication.java
@@ -0,0 +1,14 @@
+package co.edu.unicauca.matricula_financiera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaFinancieraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaFinancieraApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/co/edu/unicauca/matricula_financiera/MatriculaFinancieraApplicationTests.java
@@ -0,0 +1,14 @@
+package co.edu.unicauca.matricula_financiera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculaFinancieraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
